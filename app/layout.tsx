e510901ed67c057c292a8d2f3cbdbce5c3c10c8e
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-09-30 13:03:12 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+
+const font = Figtree({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: 'Spotify Clone',
+	description: 'Listen to music!',
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='en'>
+			<body className={font.className}>{children}</body>
+		</html>
+	);
+}

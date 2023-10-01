1febce3f5a4165216937f883d4a9a88213e3c8ab
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-10-01 13:46:20 +0200

    feat: add layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SideBar from '@/components/Sidebar';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={font.className}>{children}</body>
+			<body suppressHydrationWarning={true} className={font.className}>
+				<SideBar>{children}</SideBar>
+			</body>
 		</html>
 	);
 }

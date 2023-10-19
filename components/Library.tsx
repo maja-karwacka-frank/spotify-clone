b07e099498422e816744778f1f823981ae110826
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-10-19 15:03:19 +0200

    feat: upload modal & functionality

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,8 +3,24 @@
 import { TbPlaylist } from 'react-icons/tb';
 import { AiOutlinePlus } from 'react-icons/ai';
 
+import useAuthModal from '@/hooks/useAuthModal';
+import { useUser } from '@/hooks/useUser';
+import useUploadModal from '@/hooks/useUploadModal';
+
 const Library = () => {
-	const onClick = () => {};
+
+	const authModal = useAuthModal();
+	const uploadModal = useUploadModal();
+	const { user } = useUser();
+
+	const onClick = () => {
+		if (!user) {
+			return authModal.onOpen();
+		}
+
+		// TODO: Check for subscription
+		return uploadModal.onOpen();
+	};
 
 	return (
 		<div className='flex flex-col'>
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import AuthModal from '@/components/AuthModal';
 import { useEffect, useState } from 'react';
+
+import AuthModal from '@/components/AuthModal';
+import UploadModal from '@/components/UploadModal';
 
 const ModalProvider = () => {
 	const [isMounted, setIsMounted] = useState(false);
@@ -17,6 +19,7 @@ const ModalProvider = () => {
 	return (
 		<>
 			<AuthModal />
+			<UploadModal />
 		</>
 	);
 };

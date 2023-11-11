b1c143b47429d70cb8a28d401fda71440ef400ea
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-11-11 14:17:14 +0100

    feat: songs fetching & list display

--- components/Library.tsx
+++ components/Library.tsx
@@ -6,9 +6,14 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
+import { Song } from '@/types';
+import MediaItem from './MediaItem';
 
-const Library = () => {
+interface LibraryProps {
+	songs: Song[];
+}
 
+const Library = ({ songs }: LibraryProps) => {
 	const authModal = useAuthModal();
 	const uploadModal = useUploadModal();
 	const { user } = useUser();
@@ -35,7 +40,11 @@ const Library = () => {
 					className='text-neutral-400 cursor-pointer hover:text-white transition'
 				/>
 			</div>
-			<div className='flex flex-col gap-y-2 mt-4 px-3'>List of Songs!</div>
+			<div className='flex flex-col gap-y-2 mt-4 px-3'>
+				{songs.map((item) => (
+					<MediaItem onClick={() => {}} key={item.id} data={item} />
+				))}
+			</div>
 		</div>
 	);
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,12 +7,14 @@ import { BiSearch } from 'react-icons/bi';
 import Box from './Box';
 import SidebarItem from './SidebarItem';
 import Library from './Library';
+import { Song } from '@/types';
 
 interface SideBarProps {
+	songs: Song[]
 	children: React.ReactNode;
 }
 
-const SideBar = ({ children }: SideBarProps) => {
+const SideBar = ({ children, songs }: SideBarProps) => {
 	const pathname = usePathname();
 
 	const routes = useMemo(
@@ -43,7 +45,7 @@ const SideBar = ({ children }: SideBarProps) => {
 					</div>
 				</Box>
 				<Box className='overflow-y-auto h-full'>
-					<Library />
+					<Library songs={songs} />
 				</Box>
 			</div>
 			<main className='h-full flex-1 overflow-y-auto py-2'>{children}</main>
